3f225831a5da799bbd11cff49a062e6aed47e49f
Zach Carrera <zachcarrera@gmail.com>
2023-02-20 15:15:35 -0800

    fix code for problem 1

--- nested_dictionaries_and_lists.py
+++ nested_dictionaries_and_lists.py
@@ -1,14 +1,27 @@
 # 1. Update Values in Dictionaries and Lists
-x = [[5, 2, 3], [15, 8, 9]]
+x = [[5, 2, 3], [10, 8, 9]]
 students = [
-    {'first_name':  'Michael', 'last_name': 'Bryant'},
+    {'first_name':  'Michael', 'last_name': 'Jordan'},
     {'first_name': 'John', 'last_name': 'Rosales'}
 ]
 sports_directory = {
     'basketball': ['Kobe', 'Jordan', 'James', 'Curry'],
-    'soccer': ['Andres', 'Ronaldo', 'Rooney']
+    'soccer': ['Messi', 'Ronaldo', 'Rooney']
 }
-z = [{'x': 10, 'y': 30}]
+z = [{'x': 10, 'y': 20}]
+
+
+# 1.1 change value in x
+x[1][0] = 15
+
+# 1.2 change last_name of first student
+students[0]["last_name"] = "Bryant"
+
+# 1.3 change "Messi" to "Andres"
+sports_directory["soccer"][0] = "Andres"
+
+# 1.4 change value 20 in z to 30
+z[0]["y"] = 30
 
 
 # 2. Iterate Through a List of Dictionaries
